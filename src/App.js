2e9bae01174b37815becf47b6e5d23b42954fd0b
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-03-31 15:03:29 -0400

    created custom Heart component and header component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import HeartLogo from './heart.svg';
 import './App.css';
 
-function App() {
+const message = "cool cud";
+
+const Heart = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='heart'>
+          <img className='heart-img' src={HeartLogo} alt='heart' />
+          <p className='heart-message'>{props.msg}</p>
+      </div>
+  );
+}
+
+const Header= (props)=> {
+  return(
+    <div className="App-header">I ❤️ React</div>
+  )
+}
+
+function App() {
+  return(
+    <>
+      <Header/>
+      <Heart msg={message} />
+    </>
   );
 }
 
