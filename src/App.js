f7a33c359000d36db0707ebf43432c675c16bdcb
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-04-01 01:02:55 -0400

    testing task1 passed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import HeartLogo from './heart.svg';
+// import HeartLogo from './heart.svg';
+import Heart from './components/Heart';
+import Header from './components/Header';
 import './App.css';
 
 const message = "cool cud";
 
-const Heart = (props) => {
-  return (
-      <div className='heart'>
-          <img className='heart-img' src={HeartLogo} alt='heart' />
-          <p className='heart-message'>{props.msg}</p>
-      </div>
-  );
-}
 
-const Header = () => {
-  return(
-    <div className="App-header">I ❤️ React</div>
-  )
-}
+
+
 
 function App() {
   return(
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import '../App.css';
+
+const Header = () => {
+	return <div className='App-header'>I ❤️ React</div>;
+};
+
+// export statements allow content to be accessed in other files
+// in our project. Contrast this with the import statement, which allows content from external files to be used in the current file.
+export default Header;
